e07c3610a9dc0658f96fa1d78a2e11e60a375943
geonb <geonb@hotmail.com>
2015-12-02 21:45:40 +0100

    Create configure.inc.php

---
+++ config/configure.inc.php
@@ -0,0 +1,6 @@
+<?php
+define("DB_NAME","cube_sys");
+define("DB_USER",""); // type in your database username
+define("DB_PASSWORD",""); // type in your database password
+define("DB_HOST","127.0.0.1");
+?>
